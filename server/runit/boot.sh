b9edc7f480262e03280b1d660cbaecdb4b5f667f
Tung Le <tung@laptrinh.com.vn>
2020-09-13 07:21:54 +0700

    Nâng cấp phiên bản php 7.4.10 và xem thêm hướng dẫn readme.md để hiểu cách chạy cronjob

--- server/runit/boot.sh
+++ server/runit/boot.sh
@@ -17,7 +17,7 @@ shutdown() {
 
   # kill any other processes still running in the container
   for _pid  in $(ps -eo pid | grep -v PID  | tr -d ' ' | grep -v '^1$' | head -n -6); do
-    timeout -t 5 /bin/sh -c "kill $_pid && wait $_pid || kill -9 $_pid"
+    timeout -f 5 /bin/sh -c "kill $_pid && wait $_pid || kill -9 $_pid"
   done
   exit
 }
@@ -26,6 +26,14 @@ shutdown() {
 export > /etc/envvars
 
 PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin
+
+if [ "$JOB_ONLY" = "1" ] || [ "$JOB_ONLY" = "Yes" ] 
+then
+  # Stop service fpm
+  touch /etc/service/fpm/down
+  # allow supercronic run
+  rm -rf /etc/service/supercronic/down
+fi
 
 # run all scripts in the run_once folder
 # /bin/run-parts /etc/run_once
